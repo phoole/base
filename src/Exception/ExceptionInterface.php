415c1bfffcb81f4edd284dd86bbc21c1212c4049
Hong Zhang <phossa@126.com>
2019-09-26 16:47:31 +0800

    updated

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+/**
+ * Phoole (PHP7.2+)
+ *
+ * @category  Library
+ * @package   Phoole\Base
+ * @copyright Copyright (c) 2019 Hong Zhang
+ */
+namespace Phoole\Base\Exception;
+
+/**
+ * Exception marker
+ *
+ * @package Phoole\Base
+ * @version 1.0.0
+ */
+interface ExceptionInterface
+{
+}
