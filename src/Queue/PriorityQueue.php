5b2ca734193f2e01ffd238582323f0f88b34511b
Hong Zhang <phossa@126.com>
2019-10-25 18:09:21 +0800

    Modified PrirorityQueue

--- src/Queue/PriorityQueue.php
+++ src/Queue/PriorityQueue.php
@@ -16,7 +16,102 @@ namespace Phoole\Base\Queue;
  *
  * @package Phoole\Base
  */
-class PriorityQueue implements PriorityQueueInterface
+class PriorityQueue implements \IteratorAggregate, \Countable
 {
-    use PriorityQueueTrait;
+    /**
+     * data storage
+     *
+     * @var  array
+     */
+    protected $queue = [];
+
+    /**
+     * marker for sorted or not
+     *
+     * @var  bool
+     */
+    protected $sorted = true;
+
+    /**
+     * counter for priority
+     *
+     * @var  int
+     */
+    protected $counter = 20000000;
+
+    /**
+     * Insert data into the queue with priority
+     *
+     * @param  mixed $data
+     * @param  int  $priority priority, higher number retrieved first(-1000 - 1000)
+     * @throws \RuntimeException if priority out of range
+     * @return void
+     */
+    public function insert($data, int $priority = 0): void
+    {
+        $i = $this->getIndex($priority);
+        $this->queue[$i] = ['data' => $data, 'priority' => $priority];
+        $this->sorted = false;
+    }
+
+    /**
+     * Combine with queue and return a combined new queue
+     *
+     * @param  PriorityQueueInterface $queue
+     * @return PriorityQueueInterface
+     */
+    public function combine(PriorityQueue $queue): PriorityQueue
+    {
+        $nqueue = clone $this;
+        foreach ($queue->queue as $data) {
+            $nqueue->insert($data['data'], $data['priority']);
+        }
+        return $nqueue;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function count()
+    {
+        return count($this->queue);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIterator()
+    {
+        $this->sortQueue();
+        return new \ArrayIterator(array_column($this->queue, 'data'));
+    }
+
+    /**
+     * Generate an integer key
+     *
+     * @param  int $priority
+     * @throws \RuntimeException  priority out of range
+     * @return int
+     */
+    protected function getIndex(int $priority): int
+    {
+        if (abs($priority) > 1000) {
+            throw new \RuntimeException("Priority $priority out of range.");
+        }
+        return --$this->counter + $priority * 10000;
+    }
+
+    /**
+     * Sort the queue from higher to lower int $key
+     *
+     * @return $this
+     */
+    protected function sortQueue()
+    {
+        if (!$this->sorted) {
+            krsort($this->queue);
+            $this->sorted = true;
+        }
+        return $this;
+    }
 }
